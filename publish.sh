b63b974d820c5e3dba533926be3c215d5ed6dc8a
Simon THOBY <simonthoby@live.fr>
2015-11-23 17:18:10 +0100

    add crux ports to the menu

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "* Generating the website"
 make
@@ -6,6 +6,6 @@ if [ $? == 0 ]
 then
 	echo "* Uploading modification on Github pages"
 	ghp-import -m "new revision" -b master output && git push origin master
+  echo "* Uploading using ssh to the Raspberry Pi"
+  scp -r output/* pi:/srv/http/.
 fi
-echo "* Uploading using ssh to the Raspberry Pi"
-scp -r output/* pi:/srv/http/.
