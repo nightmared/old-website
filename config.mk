fad10f49a1bca66916146878a57ce711b04524cc
Simon THOBY <simonthoby@live.fr>
2016-01-01 11:43:51 +0100

    add 404 page and clean css

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ MD      = /usr/bin/markdown_py
 NAME    = "Simon THOBY's homepage"
 PREFIX  = output
 
-PAGES	= index about cv contact
+PAGES	= index about cv contact 404
 
 COPY	= css misc favicon.png imgs CNAME
 
--- publish.sh
+++ publish.sh
@@ -7,5 +7,5 @@ then
 	echo "* Uploading modification on Github pages"
 	ghp-import -m "new revision" -b master output && git push origin master
   echo "* Uploading using ssh to the Raspberry Pi"
-  scp -r output/* pi:/srv/http/.
+  scp -r output/* pi:/usr/local/www/srv/.
 fi
